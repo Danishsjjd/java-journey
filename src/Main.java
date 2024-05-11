cb40870a2eddc24f8cfa872f4ab369c28d2b6513
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-05-11 09:28:10 +0500

    feat: TextBox class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        var textBox1 = new TextBox();
+        var textBox2 = new TextBox();
+
+        textBox1.setText("Box 1");
+        textBox2.setText("Box 2");
+
+        System.out.println(textBox1.text);
+        System.out.println(textBox2.text);
+    }
+}
